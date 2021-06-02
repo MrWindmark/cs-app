8c158600ac1a75a2183a33d263a2625673cf29f7
mrwindmark <agentstorm@ya.ru>
2021-06-03 00:01:29 +0300

    lesson2 task3 done

--- task3.py
+++ task3.py
@@ -1,4 +1,14 @@
+import yaml
 
+data = {
+    'drinks': ['tea', 'coffee', 'juice'],
+    'total price': 340,
+    'total convert': {
+        '¥': 29.67,
+        '£': 3.28
+    }
+}
 
 if __name__ == '__main__':
-    pass
+    with open('./files/file.yaml', 'w', encoding='UTF-8') as file:
+        yaml.dump(data, file, default_flow_style=False, allow_unicode=True)
