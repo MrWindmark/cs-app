e63f2d3e9c5922655b8a82e649cdd653457eed96
mrwindmark <agentstorm@ya.ru>
2021-06-10 00:07:27 +0300

    All client.py functions tested

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ import json
 import argparse
 
 
-def set_auth_msg(_username, _password):
+def set_auth_msg(_username: str, _password: str):
     auth_msg = {
         "action": "authenticate",
-        "time": datetime.now(),
+        "time": datetime.now().strftime('%m/%d/%y %H:%M:%S'),
         "user": {
             "account_name": "",
             "password": ""
@@ -18,10 +18,10 @@ def set_auth_msg(_username, _password):
     return auth_msg
 
 
-def gen_presence_msg(_username):
+def gen_presence_msg(_username: str):
     presence_msg = {
         "action": "presence",
-        "time": datetime.now(),
+        "time": datetime.now().strftime('%m/%d/%y %H:%M:%S'),
         "type": "status",
         "user": {
             "account_name": "",
@@ -36,7 +36,7 @@ def get_logout_msg():
     return {"action": "quit"}
 
 
-def connection_check(_ip_address, _port):
+def connection_check(_ip_address: str, _port: int):
     try:
         if len(_ip_address.split('.')) == 4:
             inet_aton(_ip_address)
@@ -48,24 +48,25 @@ def connection_check(_ip_address, _port):
         return False
 
     if str(_port).isdigit():
-        if int(_port) <= 0:
+        if int(_port) <= 1023:
             print('Incorrect Port. Restart script with correct Port parameter')
             return False
     return True
 
 
-def start(_ip_address, _port):
+def start(_ip_address: str, _port: int):
     username = input('Enter your username: ')
     init_message = json.dumps(gen_presence_msg(username), indent=4, sort_keys=True, default=str)
     server_ans = send_to_server(msg=init_message, server_ip=_ip_address, port=int(_port))
     return server_ans.decode('utf-8')
 
 
-def send_to_server(msg, server_ip, port):
+def send_to_server(msg: str, server_ip: str, port: int):
     s = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
     s.connect((server_ip, port))  # Соединиться с сервером
     s.send(msg.encode('utf-8'))
     data = s.recv(1000000)
+    print(data)
     s.close()
     return data
 
