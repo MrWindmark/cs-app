41dff6d0cc3b0a7a66d07e6022b9d276da5d88a6
mrwindmark <agentstorm@ya.ru>
2021-06-15 21:24:12 +0300

    Aaaand I fix doubles in server.log

--- log/server_log_config.py
+++ log/server_log_config.py
@@ -7,16 +7,11 @@ def create_logger():
     path = os.path.abspath(os.getcwd())
     path = os.path.join(path, 'logs', 'server.log')
 
-    logging.basicConfig(
-        filename=path,
-        level=logging.DEBUG
-    )
-
     log = logging.getLogger('app.server')
+    log.setLevel(logging.DEBUG)
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     handler = TimedRotatingFileHandler(path, when='D', interval=1)
     handler.setFormatter(formatter)
-
     if log.hasHandlers():
         log.handlers.clear()
     log.addHandler(handler)
